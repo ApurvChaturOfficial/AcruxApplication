55979de8ba0aede522d04f66477c2b51013666ed
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2024-01-10 11:55:14 +0530

    Kuch

--- site/frontend/vite.config.js
+++ site/frontend/vite.config.js
@@ -8,14 +8,24 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest:  {
-        "name": "Acrux Enterprise",
-        "icons": [
+      manifest: {
+        "registerType": 'autoUpdate',
+        "name":"Acrux Enterprise",
+        "short_name":"Acrux",
+        "display": "standalone",
+        "background_color": "#451952",
+        "theme_color": "#451952",
+        "icons":[
           {
             src: "Business/Acrux/favicon_io/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          }
+          },
+          {
+            src: "Business/Acrux/favicon_io/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
         ],
       } 
     })
